432db1cbc28b04b48a52ac9793a58ab7c4e5b1ee
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-10-18 21:27:13 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,7 +11,9 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
-    project.add_backer(self)
+    if !project.backers.include?(self)
+      project.add_backer(self)
+    end
   end
 
 end
